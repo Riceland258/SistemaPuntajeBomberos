d14f1af73d70068b7df4ef9455bfbbeba38003f2
Axel Rubén Rullo <axelrullo17@gmail.com>
2025-09-21 22:17:21 -0300

    Create Axel_Utilidades.py
    funciones o contenido general que se puede llegar a ver en mas de un ABM

---
+++ Equipo Martin/Axel_Utilidades.py
@@ -0,0 +1,11 @@
+# ====================== IMPORTACIONES ======================
+import tkinter as tk
+
+# =================== CENTRADOR VENTANAS ====================
+def centrar_ventana(ventana):
+    ventana.update_idletasks()
+    ancho = ventana.winfo_width()
+    alto = ventana.winfo_height()
+    x = (ventana.winfo_screenwidth()  // 2) - (ancho // 2)
+    y = (ventana.winfo_screenheight() // 2) - (alto // 2)
+    ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
